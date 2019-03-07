dcdb53f47e62c111be9701d3846fb189d9714780
garrettfoister <garrettscottfoister@gmail.com>
2019-03-06 22:50:48 -0500

    Added Bottom Navigation Bar
    Added Bottom Nav Bar using fragments.

--- app/src/main/java/com/fourninetyfour/makeplans/MainActivity.kt
+++ app/src/main/java/com/fourninetyfour/makeplans/MainActivity.kt
@@ -15,3 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+
+
+
---
+++ app/src/main/java/com/fourninetyfour/makeplans/PlansActivity.java
@@ -0,0 +1,58 @@
+package com.fourninetyfour.makeplans;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+
+public class PlansActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plans);
+
+        //loading the default fragment
+        loadFragment(new PlansFragment());
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_search:
+                fragment = new SearchFragment();
+                break;
+
+            case R.id.navigation_plans:
+                fragment = new PlansFragment();
+                break;
+
+            case R.id.navigation_friends:
+                fragment = new FriendsFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}

762f48fb350474334eb17ec7c3019702f23ed35c
wernkej1 <35904315+wernkej1@users.noreply.github.com>
2019-05-02 13:08:43 -0400

    Update AddFriendFragment.java

--- app/src/main/java/com/fourninetyfour/makeplans/AddFriendFragment.java
+++ app/src/main/java/com/fourninetyfour/makeplans/AddFriendFragment.java
@@ -20,8 +20,6 @@ public class AddFriendFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_add_friends, null);
-        friend_email = v.findViewById(R.id.friend_email);
-
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseFirestore database = FirebaseFirestore.getInstance();
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();

5c922a79ef6f339d575cea7bdf205020edb8d1e4
wernkej1 <35904315+wernkej1@users.noreply.github.com>
2019-05-02 13:04:46 -0400

    Add Friends Layout

--- app/src/main/java/com/fourninetyfour/makeplans/FriendsFragment.java
+++ app/src/main/java/com/fourninetyfour/makeplans/FriendsFragment.java
@@ -3,15 +3,51 @@ package com.fourninetyfour.makeplans;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class FriendsFragment extends Fragment {
+
+    View v;
+    Button add_friend;
+    Button view_friend;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_friends, null);
+        setHasOptionsMenu(true);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        add_friend = v.findViewById(R.id.profile_add_friends);
+        view_friend = v.findViewById(R.id.profile_view_friends);
+
+
+        add_friend.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                Fragment fragment = new AddFriendFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.rellay1, fragment);
+                ft.addToBackStack(null);
+                ft.commit();
+        }
+
+        });
+
+        view_friend.setOnClickListener((v) -> {
+           Fragment fragment = new ViewFriendFragment();
+        });
+
         return inflater.inflate(R.layout.fragment_friends, null);
     }
 }
---
+++ app/src/main/java/com/fourninetyfour/makeplans/ViewFriendFragment.java
@@ -0,0 +1,6 @@
+package com.fourninetyfour.makeplans;
+
+import android.support.v4.app.Fragment;
+
+class ViewFriendFragment extends Fragment {
+}
